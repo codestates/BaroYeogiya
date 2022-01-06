2e270b52b276f54b4019f5f7160fb8e979e29911
amazer001 <kmo1245@naver.com>
2022-01-06 13:05:26 +0900

    Fix sequelize env setting

--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,13 @@
 'use strict';
 
-dotenv.config();
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const config = require('../config/config.js')[env];
+const config = require('../config/sequelize')[process.env.NODE_ENV];
 const db = {};
 
-console.log(config);
-
-let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
