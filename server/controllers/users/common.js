3e369c6f5373e94c3d475cbe3a3a48a578f51ce9
amazer001 <kmo1245@naver.com>
2022-01-06 15:47:14 +0900

    Add GET /user API

--- server/controllers/users/common.js
+++ server/controllers/users/common.js
@@ -50,7 +50,50 @@ module.exports = {
             })
         }
     },
+    /**
+     * 회원정보 조회 API
+     * @method GET
+     * @param { authorization } req.header
+     */
     get: (req, res) => {
+        const hashData = hash(req.headers);
+        let message = '';
+        let data = {};
+
+        // 회원정보 조회
+        if (hashData.uuid !== undefined) {
+            const { uuid, user_id } = hashData;
+            
+            user.findOne({
+                where: {
+                    uuid,
+                    user_id
+                }
+            }).then(result => {
+                if (result === null) {
+                    message = 'It\'s a user that doesn\'t exist.';
+
+                    res.status(400).send({ message });
+                } else {
+                    message = 'Success!';
+                    data.user_uuid = uuid;
+                    data.id = user_id;
+                    data.name = result.name;
+
+                    res.status(200).send({ message, data });
+                }
+            })
+        // 액세스 토큰 만료 처리
+        } else if (hashData) {
+            message = 'The access token has expired.';
+
+            res.status(401).send({ message });
+        // 액세스 토큰이 잘못됨
+        } else {
+            message = 'Check your access token.';
+
+            res.status(401).send({ message });
+        }
     },
     patch: (req, res) => {
     },
