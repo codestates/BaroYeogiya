4ecf858d35522254d2ecae11292217a4940c2a45
Brian <cowk34209@gmail.com>
2022-01-08 18:08:09 +0900

    create map api

---
+++ client/src/Pages/MainPage.js
@@ -0,0 +1,13 @@
+import '../Css/MainPage'
+
+function MainPage() {
+    return (
+        <div className="main-page-box">
+            <button>로그인</button>
+            <button>회원가입</button>
+            <button>지도</button>
+        </div>
+    )
+};
+
+export default MainPage;
---
+++ client/src/Pages/Mypage.js
@@ -0,0 +1,12 @@
+import '../Css/MyPage'
+
+function MyPage() {
+    return (
+        <div className="user-info-box">
+            <button>회원정보</button>
+            <button>나의 리뷰 목록</button>
+        </div>
+    )
+};
+
+export default MyPage;
