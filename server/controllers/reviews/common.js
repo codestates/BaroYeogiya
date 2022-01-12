06446e5961bac868a04c25b187fdad1e58e31697
amazer001 <kmo1245@naver.com>
2022-01-12 13:52:27 +0900

    Add PATCH /review API

--- server/controllers/reviews/common.js
+++ server/controllers/reviews/common.js
@@ -109,6 +109,85 @@ module.exports = {
             })
         }
     },
+    /**
+     * 리뷰 수정 API
+     * @method PATCH
+     * @param { authorization } req.headers
+     * @param { review_uuid, content, image } req.body
+     */
     patch: (req, res) => {
+        const hashData = hash(req.headers);
+        const { review_uuid, content, image } = req.body;
+        let message = '';
+        let data = {};
+
+        console.log(hashData);
+
+        // access token 조회
+        if (hashData.uuid !== undefined) {
+            // parameter 유무 확인
+            if (review_uuid === undefined || content === undefined) {
+                message = 'Check your parameter.';
+    
+                res.status(400).send({ message });
+            // 리뷰 수정
+            } else {
+                const { uuid } = hashData;
+
+                review.findOne({
+                    where: {
+                        uuid: review_uuid
+                    }
+                }).then(result => {
+                    // 리뷰 존재 여부 체크
+                    if (result === null) {
+                        message = 'Review does not exist.';
+
+                        res.status(400).send({ message });
+                    // 유저 권한 체크
+                    } else if (result.dataValues.user_uuid !== uuid) {
+                        message = 'You don\'t have permission.';
+
+                        res.status(401).send({ message });
+                    } else {
+                        review.update({
+                            content,
+                            image
+                        }, {
+                            where: {
+                                uuid: review_uuid
+                            }
+                        }).then(result2 => {
+                            console.log();
+                            if (result2[1] === 0) {
+                                message = 'Check your parameter.';
+        
+                                res.status(400).send({ message });
+                            } else {
+                                message = 'Success!';
+                                data.review_uuid = review_uuid;
+                                data.store_uuid = result.dataValues.store_uuid;
+                                data.content = content;
+                                data.image = image;
+
+                                res.status(200).send({ message, data });
+                            }
+                        }).catch(err => {
+                            console.log(err);
+                        })
+                    }
+                })
+            }
+        // 액세스 토큰 만료 처리
+        } else if (hashData) {
+            message = 'The access token has expired.';
+
+            res.status(401).send({ message });
+        // 액세스 토큰이 잘못됨
+        } else {
+            message = 'Check your access token.';
+
+            res.status(401).send({ message });
+        }
     }
 }
