b600cd018fbfa910fa0553e454c36e941d09df4a
amazer001 <kmo1245@naver.com>
2022-01-12 17:05:06 +0900

    Add POST /store API

--- server/controllers/stores/common.js
+++ server/controllers/stores/common.js
@@ -1,4 +1,6 @@
+const { v4 } = require('uuid');
 const { store } = require('../../models');
+const { hash } = require('../../utils/token');
 
 module.exports = {
     /**
@@ -34,7 +36,58 @@ module.exports = {
             })
         }
     },
+    /**
+     * 매장 등록 API
+     * @method POST
+     * @param { authorization } req.headers
+     * @param { name, latitude, longitude, image } req.body
+     */
     post: (req, res) => {
+        const hashData = hash(req.headers);
+        const { name, latitude, longitude, image } = req.body;
+        let message = '';
+        let data = {};
 
+        // access token 조회
+        if (hashData.uuid !== undefined) {
+            // parameter 유무 확인
+            if (name === undefined || latitude === undefined || longitude === undefined) {
+                message = 'Check your parameter.';
+    
+                res.status(400).send({ message });
+            // 매장 등록
+            } else {
+                store.create({
+                    uuid: v4(),
+                    address: name,
+                    latitude,
+                    longitude,
+                    image
+                }).then(result => {
+                    if (result.dataValues !== null) {
+                        message = 'Success!';
+                        data.store_uuid = result.dataValues.uuid;
+                        data.name = result.dataValues.address;
+                        data.image = result.dataValues.image;
+                        
+                        res.status(201).send({ message, data });
+                    } else {
+                        message = 'Check your parameter.';
+            
+                        res.status(400).send({ message });
+                    }
+                })
+            }
+        // 액세스 토큰 만료 처리
+        } else if (hashData) {
+            message = 'The access token has expired.';
+
+            res.status(401).send({ message });
+        // 액세스 토큰이 잘못됨
+        } else {
+            message = 'Check your access token.';
+
+            res.status(401).send({ message });
+        }
     }
 }
