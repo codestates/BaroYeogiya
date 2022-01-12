5a200cab77b3218b54f0c682a6471a3a99eeb259
Brian <cowk34209@gmail.com>
2022-01-12 13:48:46 +0900

    create and modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,52 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import MainPage from './Pages/MainPage'
 import NavBar from './Components/NavBar'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Mypage from './Pages/Mypage'
 import Map from './Pages/Map'
+import UserNavBar from './Components/UserNavBar';
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
-  const [userInfo, setUserInfo] = useState(object);
-
-  const isAuthenticated = () => {
-    // 인증에 성공한
-    axios.get(`${process.env.REACT_APP_SERVER_URL}/user/login`)
-      .then((res) => {
-        setUserInfo(res.data.accessToken);
-        // accessToken이 문자열로 들어오는지? 아니면 내가 json화 시켜줘야 하는지?
-        setIsLogin(true)
-        // userNavigate('/Map')
-        // navigate는 지호님이 만들어주면 그때 다시 연결 하는걸로
-      })
-  };
-  const handleResponse = () => {
-    isAuthenticated()
+  const [userInfo, setUserInfo] = useState(null);
+  // console.log(isLogin)
+  const handleResponse = (res) => { // accessToken을 받아 와서 state에 저장한다.
+    const userInfo = {
+      accessToken : res.data //props로 내려 주기 위해서는 객체 형태로 보내준다.
+    }
+    setUserInfo(userInfo)
+    console.log(res.data)
   };
 
-  useEffect(() => {
-    isAuthenticated()
-  },[]);
+  const handleIsLogin = (bool) => { // login 상태를 알려주는 함수
+    setIsLogin(bool) // 원래는 setUserInfo와 함께 함수를 통해서 전달 해주었는데 res 값을 따로 전해 주어야 해서 나누었다.
+  }
+
+  // useEffect(() => {
+  //   handleResponse()
+  // },[]);
 
 
   return (
     <div className="App">
       <Router>
-        <NavBar />
+        {isLogin ? <UserNavBar handleResponse={handleResponse} handleIsLogin={handleIsLogin} /> : <NavBar handleResponse={handleResponse} handleIsLogin={handleIsLogin} /> }
+        {/* isLogin 상태에 따라 로그인된 Nav가 나올지 게스트 Nav가 나올지 정했다. */}
         <Routes>
           <Route
             exact
             path="/"
             element={
-              <MainPage />
+              <MainPage isLogin={isLogin} />
             }
           ></Route> 
           <Route
             exact
             path="/mypage"
             element={
-              <Mypage />
+              <Mypage userInfo={userInfo} />
             }
           ></Route>
           <Route 
--- client/src/Components/Modals/Login.js
+++ client/src/Components/Modals/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import '../../Css/Login.css'
 
-function Login({ handleResponse, handleLoginBtn}) {
+function Login({ getResponse, handleLoginBtn, getLoging}) {
   // handleLoginBtn은 NavBar에서 오고 handleResponse는 app.js에서 온다. userInfo도 app.js에서 넘어 올거다
   // userInfo는 access_token 실은 정보
   const [LoginId, setLoginId] = useState('')
@@ -27,14 +27,15 @@ function Login({ handleResponse, handleLoginBtn}) {
       },
       withCredentials: true
     })
-    .then(res => {
-      if(res.params.id !== LoginId || res.params.pw !== LoginPw ){
-        alert('Does not match id or password.')
+    .then((res) => {
+      if(res.status === 400){
+        alert('아이디와 비밀번호의 정보가 일치하지 않습니다.')
       }
-      handleResponse();
-      console.log(handleResponse())
-      handleLoginBtn();
-      // 왜 여기서 두 함수가 같이 실행이 안될까?? 둘 중 한가지만 넣어야 잘 작동이 된다...이유는?
+      else if(res.status === 200){
+        getResponse(res);
+        getLoging(true);
+        handleLoginBtn(false);
+      }
     })
     .catch((error)=>{
       return error;
@@ -42,24 +43,23 @@ function Login({ handleResponse, handleLoginBtn}) {
   };
 
   useEffect( () => {
-    // axios.get('/user/login')
-    //   .then(res => console.log(res))
-    //   .catch()
     onClickLogin();
   },[])
 
   return (
-    <div className="login-box">
-      <h1>로그인</h1>
-      <div className="input-id">
-          <p>ID</p>
-          <input className="input-id" type="text" onChange={handleLoginId} />
+    <div className="login-entire-box">
+      <div className="login-box">
+        <h1>로그인</h1>
+        <div className="input-id">
+            <p>ID</p>
+            <input className="input-id" type="text" onChange={handleLoginId} />
+        </div>
+        <div className="input-pw">
+          <p>PW</p>
+          <input className="input-pw" type="password" onChange={handleLoginPw} />
+        </div>
+        <button className="login-bt" onClick={onClickLogin} >로그인</button>
       </div>
-      <div className="input-pw">
-        <p>PW</p>
-        <input className="input-pw" type="password" onChange={handleLoginPw} />
-      </div>
-      <button className="login-bt" onClick={onClickLogin} >로그인</button>
     </div>
   )
 }
