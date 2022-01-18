673ac42ccd3db14be96a5015428aa2570cc45483
amazer001 <kmo1245@naver.com>
2022-01-18 11:45:41 +0900

    Add GET /store/my-list API

--- server/controllers/stores/index.js
+++ server/controllers/stores/index.js
@@ -1,4 +1,5 @@
 const common = require('./common');
 const uuid = require('./uuid');
+const myList = require('./myList');
 
-module.exports = { common, uuid };
+module.exports = { common, uuid, myList };
